b74d4f224ba164d36910a24018d2d744f956e103
William Whitacre <w.whitacre@windgo.com>
2018-01-28 19:49:50 -0600

    Fixed missing git clone of wise.

--- service/get_repos.sh
+++ service/get_repos.sh
@@ -4,16 +4,22 @@ which git || {
     echo "git is required to install the dependecies of wise."
 }
 
-[ ! -d emerald ] && {
+function get_emerald () {
     git clone https://github.com/windgo-inc/emerald.git
-    cd ~/.nimble/pkgs && rm -rf 'emerald-#head'
+    (cd ~/.nimble/pkgs && rm -rf 'emerald-#head')
     (cd emerald && nimble develop) || {
         exit $?
     }
 }
 
-[ ! -d wise ] && {
+function get_wise () {
     git clone https://github.com/windgo-inc/wise.git
     exit $?
 }
+
+[ ! -d emerald ] && get_emerald
+[ ! -d emerald/.git ] && get_emerald
+
+[ ! -d wise ] && get_wise
+[ ! -d wise/.git ] && get_wise
 
